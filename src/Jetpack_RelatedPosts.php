439bf06592c8d8784b2e283d06fa906fbe2fbec4
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-04-27 13:33:34 +0000

    removed version marker

--- src/Jetpack_RelatedPosts.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-namespace Svbk\WP\Widgets;
-
-use Svbk\WP\Helpers;
-
-class Jetpack_RelatedPosts extends Jetpack\Related { } // class Foo_Widget
--- src/WhitepaperDl.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-namespace Svbk\WP\Widgets;
-
-class WhitepaperDl extends Form\Download { } 
